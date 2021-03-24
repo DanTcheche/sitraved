1ff678122b55c0a632942c077d2eb6f39e70ccb2
Dan Tcheche <dtchechenistky@devartis.com>
2021-03-23 21:14:25 -0300

    Add user model

---
+++ sitraved/apps/users/models/__init__.py
@@ -0,0 +1,2 @@
+from sitraved.apps.users.models.user import User
+__all__ = ['User']
--- sitraved/apps/users/models/user.py
+++ sitraved/apps/users/models/user.py
@@ -4,7 +4,11 @@ from django.contrib.auth.models import AbstractUser
 
 
 class BaseModel(models.Model):
+
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+
+    class Meta:
+        abstract = True
 
 
 class User(AbstractUser, BaseModel):
