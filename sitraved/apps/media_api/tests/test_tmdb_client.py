80879b6a6e87df5898f93eddb7c1138478777946
Dan Tcheche <dtchechenistky@devartis.com>
2021-03-31 00:15:26 -0300

    Add tests for search api

---
+++ sitraved/apps/media_api/tests/test_tmdb_client.py
@@ -0,0 +1,35 @@
+import json
+import os
+import responses
+import pytest
+
+from django.conf import settings
+
+from sitraved.apps.media_api.clients.TMDB_client import TMDBClient
+from sitraved.apps.media_api.strategies.search_movies_strategy import SearchMovies
+
+SEARCH_DATA = os.path.join(os.path.dirname(__file__), 'mock_data/mock_search_by_title.json')
+
+
+@pytest.mark.django_db
+class TestTMDBClient:
+
+    @responses.activate
+    def test_search_movies_by_title(self):
+        movies_strategy = SearchMovies()
+        tmdb_client = TMDBClient(movies_strategy)
+
+        response_data = json.load(open(SEARCH_DATA))
+        responses.add(
+            responses.GET, f'https://api.themoviedb.org/3/search/movie?api_key={settings.TMDB_API_KEY}&query=funny',
+            json=response_data, status=200)
+
+        response = tmdb_client.search({
+            'title': 'funny'
+        })
+
+        assert len(response['movies']) == 20
+        assert response['movies'][0]['title'] == 'Funny Face'
+        assert response['movies'][0]['year'] == '2021'
+        poster_url = 'http://image.tmdb.org/t/p/original/fD2ZAcCt4BEhKGpTxdBFoSRCn1Y.jpg'
+        assert response['movies'][0]['poster_url'] == poster_url
