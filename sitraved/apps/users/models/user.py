faa9154062936f78fcd152cd3018c80d86d0a1d3
Dan Tcheche <dtchechenistky@devartis.com>
2021-03-31 15:20:37 -0300

    Add created_at and updated_at to BaseModel

--- sitraved/apps/users/models/user.py
+++ sitraved/apps/users/models/user.py
@@ -4,8 +4,9 @@ from django.contrib.auth.models import AbstractUser
 
 
 class BaseModel(models.Model):
-
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     class Meta:
         abstract = True
