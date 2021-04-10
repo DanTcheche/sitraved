e82c081a60ee04072ae15eed7f855a2ca490d975
Dan Tcheche <dtchechenistky@devartis.com>
2021-04-10 14:07:42 -0300

    Fix errors

--- sitraved/apps/recommendations/models/__init__.py
+++ sitraved/apps/recommendations/models/__init__.py
@@ -1,6 +1,5 @@
-from sitraved.apps.recommendations.models.comment import Comment
+from sitraved.apps.recommendations.models.recommendation import Recommendation, Comment
 from sitraved.apps.recommendations.models.movie_recommendation import MovieRecommendation
 from sitraved.apps.recommendations.models.movie_recommendation_comment import MovieRecommendationComment
-from sitraved.apps.recommendations.models.recommendation import Recommendation
 
 __all__ = ['Recommendation', 'Comment', 'MovieRecommendation', 'MovieRecommendationComment']
--- sitraved/apps/recommendations/models/movie_recommendation.py
+++ sitraved/apps/recommendations/models/movie_recommendation.py
@@ -1,8 +1,7 @@
 from django.db import models
 
-from sitraved.apps.media_api.models import Movie
 from sitraved.apps.recommendations.models import Recommendation
 
 
 class MovieRecommendation(Recommendation):
-    movie = models.ForeignKey(Movie, related_name='recommendations', on_delete=models.CASCADE)
+    movie = models.ForeignKey('media_api.Movie', related_name='recommendations', on_delete=models.CASCADE)
