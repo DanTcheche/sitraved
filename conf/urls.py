517f34902788e24d525f2402e47253a865d2c9f7
Dan Tcheche <dtchechenistky@devartis.com>
2021-04-10 13:26:35 -0300

    New app setup

--- conf/urls.py
+++ conf/urls.py
@@ -5,6 +5,7 @@ from rest_framework_simplejwt import views as jwt_views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/users/', include('sitraved.apps.users.urls')),
+    path('api/recommendations/', include('sitraved.apps.recommendations.urls')),
     # JWT
     path('api/jwt/create/', jwt_views.TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/jwt/refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
