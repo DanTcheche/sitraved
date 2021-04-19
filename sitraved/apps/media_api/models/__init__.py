ad8fa01f2baf34271f497645d0b67565a1fb0e7c
Dan Tcheche <dtchechenistky@devartis.com>
2021-04-19 19:53:38 -0300

    Add genre model

--- sitraved/apps/media_api/models/__init__.py
+++ sitraved/apps/media_api/models/__init__.py
@@ -1,3 +1,4 @@
 from sitraved.apps.media_api.models.movie import Movie
+from sitraved.apps.media_api.models.movie_genre import MovieGenre
 
-__all__ = ['Movie']
+__all__ = ['Movie', 'MovieGenre']
--- sitraved/apps/media_api/models/movie.py
+++ sitraved/apps/media_api/models/movie.py
@@ -3,7 +3,8 @@ from sitraved.apps.users.models.user import BaseModel
 
 
 class Movie(BaseModel):
-    imdb_id = models.CharField(max_length=255, blank=True, null=True, unique=True, db_index=True)
+    imdb_id = models.CharField(max_length=10, blank=True, null=True, unique=True, db_index=True)
+    tmdb_id = models.CharField(max_length=10, blank=True, null=True, unique=True, db_index=True)
     title = models.CharField(max_length=255)
     plot = models.TextField(blank=True, null=True)
     language = models.CharField(max_length=255, blank=True, null=True)
@@ -13,6 +14,5 @@ class Movie(BaseModel):
 
     director = models.CharField(max_length=255, blank=True, null=True)
     raw_cast = models.CharField(max_length=5000, blank=True, null=True)
-    raw_genres = models.CharField(max_length=2000, blank=True, null=True)
     year = models.IntegerField(blank=True, null=True)
     slug = models.SlugField(max_length=250, null=True, blank=True)
