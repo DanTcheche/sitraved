bdb2a0fa6f358184504c77d956fa264f82e84b91
Dan Tcheche <dtchechenistky@devartis.com>
2021-04-20 18:48:02 -0300

    Add crew member and modify movie model

--- sitraved/apps/media_api/models/__init__.py
+++ sitraved/apps/media_api/models/__init__.py
@@ -1,4 +1,5 @@
-from sitraved.apps.media_api.models.movie import Movie
 from sitraved.apps.media_api.models.movie_genre import MovieGenre
+from sitraved.apps.media_api.models.crew_member import CrewMember
+from sitraved.apps.media_api.models.movie import Movie
 
-__all__ = ['Movie', 'MovieGenre']
+__all__ = ['Movie', 'MovieGenre', 'CrewMember']
--- sitraved/apps/media_api/models/movie.py
+++ sitraved/apps/media_api/models/movie.py
@@ -1,4 +1,6 @@
 from django.db import models
+
+from sitraved.apps.media_api.models import MovieGenre, CrewMember
 from sitraved.apps.users.models.user import BaseModel
 
 
@@ -12,7 +14,10 @@ class Movie(BaseModel):
     backdrop_url = models.CharField(max_length=255, blank=True, null=True)
     duration = models.IntegerField(blank=True, null=True)
 
-    director = models.CharField(max_length=255, blank=True, null=True)
-    raw_cast = models.CharField(max_length=5000, blank=True, null=True)
+    genres = models.ManyToManyField(MovieGenre)
+    cast = models.ManyToManyField(CrewMember)
+
+    director = models.ForeignKey(CrewMember, related_name='movies_directed', blank=True, null=True,
+                                 on_delete=models.SET_NULL)
     year = models.IntegerField(blank=True, null=True)
     slug = models.SlugField(max_length=250, null=True, blank=True)
