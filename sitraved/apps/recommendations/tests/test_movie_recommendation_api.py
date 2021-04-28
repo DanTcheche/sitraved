fc2b34ea8668b97f38176311fb433b682915ff82
Dan Tcheche <dtchechenistky@devartis.com>
2021-04-28 01:52:10 -0300

    Add test

--- sitraved/apps/recommendations/tests/test_movie_recommendation_api.py
+++ sitraved/apps/recommendations/tests/test_movie_recommendation_api.py
@@ -19,16 +19,14 @@ class TestMovieRecommendationViewSet:
     @pytest.fixture
     def set_up(self):
         self.client = APIClient()
+        self.movie_tmdb_id = 664596
 
     def test_create_movie_recommendation(self, set_up):
-
         self.__login(set_up)
 
-        movie_tmdb_id = 664596
-
-        self.__set_up_data(movie_tmdb_id)
+        self.__set_up_data(self.movie_tmdb_id)
         params = {
-            'tmdb_id': movie_tmdb_id
+            'tmdb_id': self.movie_tmdb_id
         }
         response = self.client.post('/api/recommendations/movies/', params)
 
@@ -38,14 +36,11 @@ class TestMovieRecommendationViewSet:
         assert MovieRecommendation.objects.all().count() == 1
 
     def test_cannot_create_same_recommendation_twice(self, set_up):
-
         self.__login(set_up)
 
-        movie_tmdb_id = 664596
-
-        self.__set_up_data(movie_tmdb_id)
+        self.__set_up_data(self.movie_tmdb_id)
         params = {
-            'tmdb_id': movie_tmdb_id
+            'tmdb_id': self.movie_tmdb_id
         }
         self.client.post('/api/recommendations/movies/', params)
 
@@ -55,6 +50,15 @@ class TestMovieRecommendationViewSet:
         response = response.json()
         assert not response['success']
         assert response['message'] == 'You have already recommended this movie.'
+
+    def test__cannot_create_movie_recommendation_not_logged_in(self, set_up):
+        self.__set_up_data(self.movie_tmdb_id)
+        params = {
+            'tmdb_id': self.movie_tmdb_id
+        }
+        response = self.client.post('/api/recommendations/movies/', params)
+
+        assert response.status_code == 401, str(response.content)
 
     def __login(self, set_up):
         UserFactory(username='testuser', email='test@user.com', password='correctpassword')
