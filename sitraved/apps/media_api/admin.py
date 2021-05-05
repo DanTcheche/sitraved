30dafc247adc7311d46b5b20a2e1cab03ac95a5c
Dan Tcheche <dtchechenistky@devartis.com>
2021-05-05 14:34:10 -0300

    Add admin for all models

--- sitraved/apps/media_api/admin.py
+++ sitraved/apps/media_api/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
 
-# Register your models here.
+from sitraved.apps.media_api.models import CrewMember, Language, MovieCrewMember, MovieGenre, Movie
+
+
+@admin.register(CrewMember)
+class CrewMemberAdmin(admin.ModelAdmin):
+    list_filter = ('created_at',)
+
+
+@admin.register(Language)
+class LanguageAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(MovieCrewMember)
+class MovieCrewMemberAdmin(admin.ModelAdmin):
+    list_filter = ('created_at',)
+
+
+@admin.register(MovieGenre)
+class MovieGenreAdmin(admin.ModelAdmin):
+    list_filter = ('created_at',)
+
+
+@admin.register(Movie)
+class MovieAdmin(admin.ModelAdmin):
+    list_filter = ('created_at',)
