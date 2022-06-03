bfb229dc10bb831c4e84b31672bd142b27eda459
Dan Tcheche <dtcheche@gmail.com>
2022-06-03 12:48:13 -0300

    Setup secret in env.

--- conf/settings/base.py
+++ conf/settings/base.py
@@ -29,7 +29,7 @@ except FileNotFoundError:
     pass
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ''
+SECRET_KEY = env('SECRET_KEY', default="jfzs8+ry$p^_kr#yo*neqnr(@o3c8*n_1bu+cxh1c0n(5+)*f+")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env('DEBUG', default=False)
