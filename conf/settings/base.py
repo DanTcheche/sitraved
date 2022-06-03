b315d7670c433ca75903222499b5b7de129ba0bf
Dan Tcheche <dtcheche@gmail.com>
2022-06-03 12:22:02 -0300

    Update base.py

--- conf/settings/base.py
+++ conf/settings/base.py
@@ -29,7 +29,7 @@ except FileNotFoundError:
     pass
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'jfzs8+ry$p^_kr#yo*neqnr(@o3c8*n_1bu+cxh1c0n(5+)*f+'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env('DEBUG', default=False)
